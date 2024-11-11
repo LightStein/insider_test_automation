6599fd62fe75b456fa76720d84083d3e70997af3
Anri Giorganashvili | Thomas Miller <anri.giorganashvili.external@worldline.com>
2024-11-12 02:03:14 +0400

    set the default resolution

--- selenium-project/test_insider_careers.py
+++ selenium-project/test_insider_careers.py
@@ -17,6 +17,7 @@ if headless:
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--window-size=1920,1080")
 
 # Initialize the WebDriver globally
 try:
